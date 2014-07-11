4d70b2b4ec31bed9a206962ea89b99787a0e9dcc
Andreas Lappe <a.lappe@zimmer7.com>
2014-07-11 16:21:29 +0200

    Add apt run on ubuntu/debian for current python

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures beaver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
 recipe           'beaver', 'Installs and configures beaver to ship logs to logstash'
-%w{ python }.each do |cookbook|
+%w{ python apt }.each do |cookbook|
   depends cookbook
 end
 
